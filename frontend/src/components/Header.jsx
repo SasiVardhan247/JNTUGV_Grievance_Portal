1d48f229d2a898238628157d9335ba348628fee0
SasiVardhan247 <sasivardhan247@gmail.com>
2023-10-17 18:47:15 +0530

    setup

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <header id="header" className="fixed-top">
+        <div className="container d-flex align-items-center justify-content-between">
+          <h1 className="logo">
+            <a href="https://jntugv.edu.in">
+              <img src="https://dhondi.ai/logos/jntugv.png" alt="logo" />
+            </a>
+          </h1>
+
+          <nav id="navbar" className="navbar">
+            <ul>
+              <li>
+                <a className="active" href="index.html">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="our-story.html">Our Story</a>
+              </li>
+              <li>
+                <a href="events.html">Events</a>
+              </li>
+              <li>
+                <a href="gallery.html">Gallery</a>
+              </li>
+              <li className="dropdown">
+                <a href="#">
+                  <span>Drop Down</span> <i className="bi bi-chevron-down"></i>
+                </a>
+                <ul>
+                  <li>
+                    <a href="#">Drop Down 1</a>
+                  </li>
+                  <li className="dropdown">
+                    <a href="#">
+                      <span>Deep Drop Down</span>{" "}
+                      <i className="bi bi-chevron-right"></i>
+                    </a>
+                    <ul>
+                      <li>
+                        <a href="#">Deep Drop Down 1</a>
+                      </li>
+                      <li>
+                        <a href="#">Deep Drop Down 2</a>
+                      </li>
+                      <li>
+                        <a href="#">Deep Drop Down 3</a>
+                      </li>
+                      <li>
+                        <a href="#">Deep Drop Down 4</a>
+                      </li>
+                      <li>
+                        <a href="#">Deep Drop Down 5</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href="#">Drop Down 2</a>
+                  </li>
+                  <li>
+                    <a href="#">Drop Down 3</a>
+                  </li>
+                  <li>
+                    <a href="#">Drop Down 4</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="contact.html">Contact</a>
+              </li>
+            </ul>
+            <i className="bi bi-list mobile-nav-toggle"></i>
+          </nav>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;

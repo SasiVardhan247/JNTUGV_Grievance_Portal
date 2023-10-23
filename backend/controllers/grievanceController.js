771b7a1139b8e6b32b5d35dfcc3c64e7c5130316
SasiVardhan247 <sasivardhan247@gmail.com>
2023-10-23 13:54:09 +0530

    updated acknoledgement number functionality

--- backend/controllers/grievanceController.js
+++ backend/controllers/grievanceController.js
@@ -1,22 +1,42 @@
 const Grievance = require('../models/grievance.model');
 const nodemailer = require("nodemailer");
+const crypto = require('crypto');
+// const { v4: uuidv4 } = require('uuid');
 
 // const transporter = nodemailer.createTransport({
 //     host: "smtp-mail.outlook.com",
 //     port: 587,
 //     auth: {
-//       user: process.env.AUTH_EMAIL,
-//       pass: process.env.AUTH_PASS,
-//     },
-//   });
+    //       user: process.env.AUTH_EMAIL,
+    //       pass: process.env.AUTH_PASS,
+    //     },
+    //   });
 const transporter = nodemailer.createTransport({
     service:'Gmail',
     auth:{
         user:process.env.AUTH_EMAIL,
         pass:process.env.AUTH_PASS
-    },
+    },
 });
 
+const generateUniqueAcknowledgmentNumber = async() => {
+    const timestamp = Date.now().toString();
+    // console.log(timestamp)
+    const randomBytes = crypto.randomBytes(4).toString('hex'); // You can adjust the number of bytes for randomness
+
+    const acknowledgmentNumber = timestamp + randomBytes;
+    const hash = crypto.createHash('sha256').update(acknowledgmentNumber).digest('hex');
+
+    return hash.slice(0,5); // Adjust the length as needed
+}
+
+//This is 36 long ack number ex:86531862-bc94-4bab-aa15-1c233efeb80e will always be unique use this function when the requests are too huge
+// const generateUniqueAcknowledgmentNumber = async() => {
+//     const acknowledgmentNumber = uuidv4();
+//     return acknowledgmentNumber;
+// }
+
+// console.log(generateUniqueAcknowledgmentNumber())
 exports.getGrievance = async(req,res) =>{
     try{
         const grievances = await Grievance.find({})
@@ -28,22 +48,24 @@ exports.getGrievance = async(req,res) =>{
 };
 exports.applyGrievance = async(req,res) =>{
     try{
-        const pad = (num, size) => {
-            let s = num + "";
-            while (s.length < size) {
-              s = "0" + s;
-            }
-            return s;
-        };
-        const getCountWithLeadingZeros = async () => {
-            const count = await Grievance.countDocuments({});
-            const incrementedCount = count + 1;
-            const countString = pad(incrementedCount, 5);
-            return countString;
-        };
+        // const pad = (num, size) => {
+        //     let s = num + "";
+        //     while (s.length < size) {
+        //       s = "0" + s;
+        //     }
+        //     return s;
+        // };
+        // const getCountWithLeadingZeros = async () => {
+        //     const count = await Grievance.countDocuments({});
+        //     const incrementedCount = count + 1;
+        //     const countString = pad(incrementedCount, 5);
+        //     return countString;
+        // };
         const {email} = req.body
         console.log(req.body);
-        const formattedCount = await getCountWithLeadingZeros();
+        // const formattedCount = await getCountWithLeadingZeros();
+        // const formattedCount = await generateUniqueAcknowledgmentNumber();
+        const formattedCount = await generateUniqueAcknowledgmentNumber();
         await Grievance.create({...req.body,acknoledgementId:`SGRNO${formattedCount}`,status:'pending', grievanceResponseTime : null , grievanceReply : null})
         const mailOptions = {
             from: process.env.AUTH_EMAIL,
